9b92347cc155e165ea56b0b2b3dae2563b0dc26b
OmBad1033 <om.badavanache05@gmail.com>
2025-10-05 20:29:48 +0530

    Frontend setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "./theme-toggle";
 
 export default function Home() {
-  return <h1>Hello NextJs</h1>;
+  return (
+    <div
+      className="min-h-screen flex items-center justify-center bg-white 
+    dark:bg-black text-black dark:text-white transition-all duration 300"
+    >
+      <div className="max-w-3xl text-center space-y-10">
+        <h1 className="text-6xl font-semibold">Collab Learn dark and light modes</h1>
+        <p>In TypeScript (ESM modules), you can’t use the CommonJS require() syntax — it’s not valid in ES module contexts.
+        Since your tailwind.config.ts is using export default, it’s treated as an ES module, and TypeScript disallows require().</p>
+        <div className="space-x-2">
+          <Button> Light</Button>
+          <Button variant="secondary"> Dark</Button>
+        </div>
+        <ThemeToggle/>
+      </div>
+    </div>
+  );
 }
